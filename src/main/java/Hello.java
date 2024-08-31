02c8444eca9de2ebe7ece3be4740e81306d1aaaa
Solomon0519 <ng.eswen1@gmail.com>
2024-09-01 02:39:59 +0800

    GUI-Update-1

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,4 +1,11 @@
+/**
+ * Represents class to say Hello
+ */
 public class Hello {
+    /**
+     * Prints Hello World
+     * @param args arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
